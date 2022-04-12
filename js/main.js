d51fae42d6481c77e6928e0a6467e20ee68a6aee
Oneill <yujinoneill@gmail.com>
2022-04-12 12:12:15 +0900

    move a chart to other js file

--- js/main.js
+++ js/main.js
@@ -5,20 +5,3 @@ function openNav() {
 function closeNav() {
     $(".side-nav").css("width", "0px");
 }
-
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ['Done', 'Not yet'],
-        datasets: [{
-            label: 'April Plan Progress',
-            data: [60, 40],
-            backgroundColor: [
-                '#A3CEF1',
-                '#E7ECEF'
-            ],
-            hoverOffset: 4
-        }]
-    }
-});
