1d8e27a66c0ce6f4b88a6f172690d2ddd481e24a
Oneill <yujinoneill@gmail.com>
2022-04-14 21:36:42 +0900

    add event listener to show cheerful message

--- js/home.js
+++ js/home.js
@@ -14,3 +14,36 @@ const myChart = new Chart(ctx, {
         }]
     }
 });
+
+const check1 = document.querySelector("#check1");
+const check2 = document.querySelector("#check2");
+const check3 = document.querySelector("#check3");
+const check4 = document.querySelector("#check4");
+
+check1.addEventListener("change", function () {
+    if (check1.checked == true) {
+        alert("You did a good job!");
+    }
+})
+
+check2.addEventListener("change", function () {
+    if (check2.checked == true) {
+        alert("You did a good job!");
+    }
+})
+
+check3.addEventListener("change", function () {
+    if (check3.checked == true) {
+        alert("You did a good job!");
+    }
+})
+
+check4.addEventListener("change", function () {
+    if (check4.checked == true) {
+        alert("You did a good job!");
+    }
+})
+
+
+
+
