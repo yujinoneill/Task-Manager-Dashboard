6b65ba8b4eda4dbc8b4e972e82b4d18f209b2a57
Oneill <yujinoneill@gmail.com>
2022-04-26 15:25:24 +0900

    refactor the hard-coded codes using for...of

--- js/data.js
+++ js/data.js
@@ -1,20 +1,7 @@
-const card1 = document.querySelector("#card1");
-const card2 = document.querySelector("#card2");
-const card3 = document.querySelector("#card3");
-const card4 = document.querySelector("#card4");
+const cardImgs = document.querySelectorAll('.card img');
 
-card1.addEventListener("click", function () {
-    document.querySelector("#card1 img").classList.toggle("img-scale");
-})
-
-card2.addEventListener("click", function () {
-    document.querySelector("#card2 img").classList.toggle("img-scale");
-})
-
-card3.addEventListener("click", function () {
-    document.querySelector("#card3 img").classList.toggle("img-scale");
-})
-
-card4.addEventListener("click", function () {
-    document.querySelector("#card4 img").classList.toggle("img-scale");
-})
+for (let img of cardImgs) {
+    img.addEventListener('click', function () {
+        this.classList.toggle("img-scale");
+    })
+}
